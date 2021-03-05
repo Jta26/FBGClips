d9a5b0ed3bd404a4c859ac3c8bd56bacc5991e1d
Jta26 <jta26@pitt.edu>
2021-03-05 13:24:39 -0500

    Facebook Webhook Challenge

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+const webhooks = require('./routes/webhooks');
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
+
+
+app.use('/webhooks', (req, res) => {
+
+    if (req.query['hub.mode'] == 'subscribe' 
+        && req.query['hub.verify_token'] == 'SuperSecretToken'
+    ) {
+        res.send(req.query['hub.challenge']);
+    }
+    else {
+        res.sendStatus(400);
+    }
+}); 
 
 
 app.listen(port, () => {
