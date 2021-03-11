e4244c1f806365d2c3f279fd9374bb438fe4478e
Jta26 <jta26@pitt.edu>
2021-03-10 20:36:42 -0500

    Discord bot notifications and embeds

--- services/discordbot.js
+++ services/discordbot.js
@@ -8,57 +8,56 @@ bot.on('ready', () => {
     console.log('Discord Bot Connected Successfully');
 });
 
-const notifyClipPosted = async (video) => {
+const notifyClipPosted = async (notification) => {
     try {
-        console.log(process.env.DISCORD_USER_ID);
         const user = await bot.users.fetch(process.env.DISCORD_USER_ID);
         if (user) {
-            user.send(JSON.stringify(video));
+            const embed = makeDiscordEmbed(notification);
+            user.send(embed);
         }
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
     }
 }
 
+const makeDiscordEmbed = (notification) => {
+    const notificationEmbed = new Discord.MessageEmbed().setColor('#2D88FF')
+        .setAuthor('Facebook Gaming Clips')
+        .setTitle(notification.content)
+        .setDescription(notification.message)
+        .setImage(notification.image)
+        .setURL(notification.fbgclips_url)
+        .addFields(
+            {name: 'FBG Clips URL', value: notification.fbgclips_url},
+            {name: 'Facebook Gaming URL', value: notification.fb_url},
+        );
 
-const assembleNotification = (body) => {
-    // If a new video was posted.
-    if (body.field == "videos") {
-        const videoId = body.value.id;
-        
+    return notificationEmbed;
+}
 
-    }
-    else if (body.field == 'live_videos') {
-
-    }
-    else if (body.field == 'feed') {
-
-    }
-    else {
-        // unknown post type
+const notifyMultiple = (posts) => {
+    for (let post of posts) {
+        const notification = assembleNotification(post);
+        notifyClipPosted(notification);
     }
 }
 
-const shouldNotify = (body) => {
-
-    if (body.field == "videos") {
-
+const assembleNotification = (post) => {
+    if (!post.permalink_url) {
+        post.permalink_url = `https://facebook.com/${post.id}`;
     }
-    else if (body.field == 'live_videos') {
-
+    return {
+        message: `${post.from.name} just posted to their page.`,
+        content: post.message || null,
+        fb_url: post.permalink_url,
+        fbgclips_url: `https://FBGClips.com/posts/${post.id}`,
+        image: post.picture || post.full_picture || null
     }
-    else if (body.field == 'feed') {
-
-    }
-    else {
-        // unknown post type
-    }
-
-
 }
+
 
 
 module.exports = {
-    notifyClipPosted
+    notifyClipPosted,
+    notifyMultiple
 }
