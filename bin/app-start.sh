2505625dc0297053ca24849926ec19d592c5fe5c
Jta26 <jta26@pitt.edu>
2021-03-18 19:08:24 -0400

    cd to directory on start and stop

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "starting application"
+cd ~/FBGClips
 ~/.nvm/versions/node/v14.16.0/bin/pm2 start server.js
--- bin/app-stop.sh
+++ bin/app-stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "stopping application"
+cd ~/FBGClips
 ~/.nvm/versions/node/v14.16.0/bin/pm2 stop server
